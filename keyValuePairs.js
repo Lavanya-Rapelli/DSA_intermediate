c33b164c99e557004c0c0a831a8cfbd006b9f04d
Lavanya Rapelli <lavanyarapelli555@gmail.com>
2024-09-28 14:07:28 +0530

    key value pairs

---
+++ keyValuePairs.js
@@ -0,0 +1,11 @@
+//count occurences
+function countOccurrences(arr) {
+    let counts = {};
+    for (let num of arr) {
+        counts[num] = (counts[num] || 0) + 1;
+    }
+    return counts;
+}
+
+console.log(countOccurrences([1, 2, 2, 3, 3, 3]));  
+// Output: { '1': 1, '2': 2, '3': 3 }
